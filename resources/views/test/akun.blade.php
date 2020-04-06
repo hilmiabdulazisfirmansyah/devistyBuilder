44d700201f1a3268dc8b3bdc93be4f8becb2f114
hilmiabdulazisfirmansyah <hilmiabdulazisfirmansyah@gmail.com>
2020-04-06 16:36:13 +0700

    test

--- resources/views/test/akun.blade.php
+++ resources/views/test/akun.blade.php
@@ -76,7 +76,7 @@ function siswa($nama)
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Peserta Didik</title>
+	<title>Peserta Didik </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
